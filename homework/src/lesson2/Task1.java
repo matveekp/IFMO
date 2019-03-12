0135ff9f97c67a51f5cc779d8605ad130167255d
matveevkp <matveevkp@gmail.com>
2019-03-12 14:38:44 +0300

    +

--- homework/src/lesson2/Task1.java
+++ homework/src/lesson2/Task1.java
@@ -1,4 +1,40 @@
 package lesson2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
+
+//    Задача 1*
+//    В три переменные a, b и c явно записаны программистом три целых попарно неравных между собой числа.
+//    Создать программу, которая переставит числа в переменных таким образом, чтобы при выводе на
+//    экран последовательность a, b и c оказалась строго возрастающей.
+
+    public static void main(String[] args) {
+
+    int a,b,c;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Введите 3 числа через пробел");
+    String[] list = scanner.nextLine().split(" ");
+
+    int[] array = new int[list.length];
+
+        for (int i = 0; i < array.length ; i++) {
+            array[i] = Integer.parseInt(list[i]);
+        }
+
+    Arrays.sort(array);
+
+
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+
+
+
+    }
+
 }
---
+++ homework/src/lesson2/Task2.java
@@ -0,0 +1,22 @@
+package lesson2;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+//        Задача 2
+//        Создайте программу, выводящую на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60 ….
+
+        int start = 90;
+
+        while (start>0) {
+
+            System.out.print(start + " ");
+            start-=5;
+
+
+        }
+
+
+
+    }
+}
