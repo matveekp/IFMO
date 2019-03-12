2492326d222e5e00871c0c882555e4399141edc0
matveevkp <matveevkp@gmail.com>
2019-03-12 20:25:40 +0300

    +

--- homework/src/lesson2/Task2.java
+++ homework/src/lesson2/Task2.java
@@ -6,11 +6,11 @@ public class Task2 {
 //        Задача 2
 //        Создайте программу, выводящую на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60 ….
 
-        int start = 90;
+        int i = 90;
 
-        while (start>0) {
-            System.out.print(start + " ");
-            start-=5;
+        while (i>0) {
+            System.out.print(i + " ");
+            i-=5;
         }
     }
 }
