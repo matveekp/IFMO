0fa590660f9451746d0d396f5472d40c02824a49
matveevkp <matveevkp@gmail.com>
2019-03-14 14:08:52 +0300

    +

---
+++ homework/src/lesson3/Task5.java
@@ -0,0 +1,35 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class Task5 {
+
+//    1) Найти в строке указанную подстроку и заменить ее на новую.
+//    Строку, ее подстроку для замены и новую подстроку вводит пользователь.
+
+    public static void main(String[] args) {
+
+        String string;
+        String findString;
+        String changeString;
+        String newString;
+
+        StringBuilder sb = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите строку");
+        string = scanner.nextLine();
+//        string = "Строку, ее подстроку для замены и новую подстроку вводит пользователь."; //отладка
+
+        System.out.println("Введите подстроку, которую нужно заменить");
+        findString = scanner.nextLine();
+
+        System.out.println("Введите новую подстроку");
+        changeString = scanner.nextLine();
+
+        newString = string.replace(findString, changeString);
+
+        System.out.println(newString);
+
+    }
+}
