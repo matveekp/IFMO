02ad274ad0a8d48cda6a814eef8bac91f88d3a1d
matveevkp <matveevkp@gamil.com>
2019-04-03 21:29:17 +0300

    +

--- lessons/lesson12/messageApp/Connection.java
+++ lessons/lesson12/messageApp/Connection.java
@@ -1,7 +1,30 @@
 package lesson12.messageApp;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Connection {
 
+    private Socket socket;
+    private ObjectInputStream objIn;
+    private ObjectOutputStream objOut;
+
+    public Connection(Socket socket) throws IOException {
+        this.objIn = new ObjectInputStream(socket.getInputStream());
+        this.objOut = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    Message readMessage() throws IOException, ClassNotFoundException {
+        Message message = (Message) objIn.readObject();
+        return message;
+    }
+
+    public void writeObject(Message message) throws IOException {
+        objOut.writeObject(message);
+        objOut.flush();
+            }
 
 
 
