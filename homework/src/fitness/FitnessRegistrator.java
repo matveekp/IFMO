bf6ded9b8eb510b3e96b9166a437e9b495f4e90a
matveevkp <matveevkp@gamil.com>
2019-04-12 21:50:45 +0300

    +

--- homework/src/fitness/FitnessRegistrator.java
+++ homework/src/fitness/FitnessRegistrator.java
@@ -7,6 +7,8 @@ public class FitnessRegistrator {
     private ArrayList<Human> inPool = new ArrayList<>();
     private ArrayList<Human> inGroup = new ArrayList<>();
 
+    //поменять листы на treeset or hashset for comparator
+
     public void add(Human human, FitnessServiceEnumeration type){
         if (FitnessServiceEnumeration.GYM.equals(type)) {
             //проверки отдельными методами
