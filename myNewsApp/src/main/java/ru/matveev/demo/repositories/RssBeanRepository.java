ac3882c22ed5ee0f866ac4ee8b5e97e17c5af561
matveevkp <matveevkp@gmail.com>
2019-05-28 17:26:25 +0300

    +

--- myNewsApp/src/main/java/ru/matveev/demo/repositories/RssBeanRepository.java
+++ myNewsApp/src/main/java/ru/matveev/demo/repositories/RssBeanRepository.java
@@ -3,5 +3,4 @@ package ru.matveev.demo.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.matveev.demo.entity.RssBean;
 
-public interface RssBeanRepository extends CrudRepository<RssBean, Integer> {
-}
+public interface RssBeanRepository extends CrudRepository<RssBean, Integer> { }
