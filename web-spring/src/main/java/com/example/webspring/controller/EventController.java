eab1ef87cc779fa67d15cc2ed9d7fd70a28ae27c
matveevkp <matveevkp@gmail.com>
2019-05-28 13:21:01 +0300

    +

--- web-spring/src/main/java/com/example/webspring/controller/EventController.java
+++ web-spring/src/main/java/com/example/webspring/controller/EventController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static com.example.webspring.specifications.EventSpecification.eventByTitle;
 
 @Controller
 public class EventController {
@@ -25,7 +28,31 @@ public class EventController {
     private String submitForm(@ModelAttribute Event event, Model model) {
 
         eventRepository.save(event);
+        model.addAttribute("addInfo", event.getEventTitle()); //передаем оратно название события
         return "add_event";
+    }
+
+    @RequestMapping(value = "/event/info", method = RequestMethod.GET)
+    public String infoForm(Model model){
+        model.addAttribute("events", eventRepository.findAll());
+
+        return "event_info";
+    }
+
+    @RequestMapping(value = "/event/info", method = RequestMethod.POST)
+    private String showEventInfo(@RequestParam(name = "eventId", required = true, defaultValue = "1") int eventId,
+                                 Model model){
+
+
+        eventRepository.findOne(eventByTitle("title"));
+
+
+        model.addAttribute("event", eventRepository.findById(eventId));
+
+
+
+
+        return "event_info";
     }
 
 
--- web-spring/src/main/java/com/example/webspring/repositories/EventRepository.java
+++ web-spring/src/main/java/com/example/webspring/repositories/EventRepository.java
@@ -1,9 +1,15 @@
 package com.example.webspring.repositories;
 
 import com.example.webspring.entity.Event;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface EventRepository extends CrudRepository<Event, Integer> {
-    //все основные методы уже реализованы, нужно добавить тольк сложные если нужно
+public interface EventRepository extends CrudRepository<Event, Integer>, JpaSpecificationExecutor<Event> {
+    //все основные методы уже реализованы в Crud, нужно добавить тольк сложные если нужно
+
+    // интерфейс со спецификациями JpaSpecificationExecutor
+
+
+
 
 }
