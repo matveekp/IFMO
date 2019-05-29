bd96798c7d2ff455c6f3e476b5dcbe932f51bfe9
matveevkp <matveevkp@gamil.com>
2019-05-29 22:00:02 +0300

    +

--- parse-server/src/main/java/com/example/parseserver/SecurityConfig.java
+++ parse-server/src/main/java/com/example/parseserver/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.example.parseserver;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
